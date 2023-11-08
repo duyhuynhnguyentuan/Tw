c75446ebf824a15c604247bbc7ed0583712cd284
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2023-11-08 23:18:01 +0700

    bcrypt hash

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,8 @@ const mongoose  = require('mongoose')
 
 const validator = require('validator')
 
+const bcrypt = require('bcryptjs')
+
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -71,6 +73,15 @@ userSchema.methods.toJSON = function () {
 
     return userObject
 }
+//To Hash the password
+
+userSchema.pre('save', async function(next) {
+    const user = this
+    if (user.isModified('password')){
+        user.password =await bcrypt.hash(user.password, 8)
+    }
+    next()
+})
 
 const User = mongoose.model('User', userSchema)
 
